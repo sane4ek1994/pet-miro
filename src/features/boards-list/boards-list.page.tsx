a96a2873e705676ebe803129b37adbd7c59c9bff
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-07-11 06:09:47 +0300

    added env file and settings backend

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,13 @@
 import { href, Link } from 'react-router'
 import { ROUTES } from '@/shared/model/routes.ts'
+import { CONFIG } from '@/shared/model/config.ts'
 
 export { BoardCard } from './board-card'
 
 function BoardListPage() {
   return (
     <div>
-      <h1>BoardListPage</h1>
+      <h1>BoardListPage {CONFIG.API_BASE_URL}</h1>
       <Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board - 1</Link>
     </div>
   )
