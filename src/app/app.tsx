910c88a034afba82cd04aeb8a2f235351778a86d
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-07-11 08:50:24 +0300

    feat: added tailwind and shadCN lib, settings config

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,7 +7,7 @@ export function App() {
 
   const isAuthPage = location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER
   return (
-    <div>
+    <div className='bg-gray-100'>
       {!isAuthPage && <AppHeader />}
       <Outlet />
     </div>
